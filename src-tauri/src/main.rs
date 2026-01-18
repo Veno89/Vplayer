f2a189d1c114fa0b251fb97ab6a8e5b9c1457180
Veno89 <zhitnick@gmail.com>
2026-01-18 13:43:14 +0100

    Fix: Enable updater plugin in main.rs & sync Cargo.toml version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -81,7 +81,7 @@ fn main() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_log::Builder::default().build())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        // .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .setup(|app| {
             info!("Initializing VPlayer application");
